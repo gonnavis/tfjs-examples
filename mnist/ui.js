b42bcb437420d5deb565717c189e4629a4526caf
gonnavis <gonnavis@163.com>
2023-12-11 18:11:57 +0800

    branch: vis/master

--- mnist/ui.js
+++ mnist/ui.js
@@ -66,13 +66,13 @@ export function plotLoss(batch, loss, set) {
   const series = set === 'train' ? 0 : 1;
   lossValues[series].push({x: batch, y: loss});
   const lossContainer = document.getElementById('loss-canvas');
-  tfvis.render.linechart(
-      lossContainer, {values: lossValues, series: ['train', 'validation']}, {
-        xLabel: 'Batch #',
-        yLabel: 'Loss',
-        width: 400,
-        height: 300,
-      });
+  // tfvis.render.linechart(
+  //     lossContainer, {values: lossValues, series: ['train', 'validation']}, {
+  //       xLabel: 'Batch #',
+  //       yLabel: 'Loss',
+  //       width: 400,
+  //       height: 300,
+  //     });
   lossLabelElement.innerText = `last loss: ${loss.toFixed(3)}`;
 }
 
@@ -81,14 +81,14 @@ export function plotAccuracy(batch, accuracy, set) {
   const accuracyContainer = document.getElementById('accuracy-canvas');
   const series = set === 'train' ? 0 : 1;
   accuracyValues[series].push({x: batch, y: accuracy});
-  tfvis.render.linechart(
-      accuracyContainer,
-      {values: accuracyValues, series: ['train', 'validation']}, {
-        xLabel: 'Batch #',
-        yLabel: 'Accuracy',
-        width: 400,
-        height: 300,
-      });
+  // tfvis.render.linechart(
+  //     accuracyContainer,
+  //     {values: accuracyValues, series: ['train', 'validation']}, {
+  //       xLabel: 'Batch #',
+  //       yLabel: 'Accuracy',
+  //       width: 400,
+  //       height: 300,
+  //     });
   accuracyLabelElement.innerText =
       `last accuracy: ${(accuracy * 100).toFixed(1)}%`;
 }
